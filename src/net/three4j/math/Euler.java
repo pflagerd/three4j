2d7684ce5b0ca2010064dd04478f00b7465efd8a
Daniel P Pflager <daniel.pflager@gmail.com>
2021-01-06 23:00:12 -0800

    Almost ready for test.

--- src/net/three4j/math/Euler.java
+++ src/net/three4j/math/Euler.java
@@ -118,197 +118,214 @@ public class Euler {
 		return this;
 	}
 
-//	setFromRotationMatrix( m, order, update ) {
-//
-//		const clamp = MathUtils.clamp;
-//
-//		// assumes the upper 3x3 of m is a pure rotation matrix (i.e, unscaled)
-//
-//		const te = m.elements;
-//		const m11 = te[ 0 ], m12 = te[ 4 ], m13 = te[ 8 ];
-//		const m21 = te[ 1 ], m22 = te[ 5 ], m23 = te[ 9 ];
-//		const m31 = te[ 2 ], m32 = te[ 6 ], m33 = te[ 10 ];
-//
-//		order = order || this._order;
-//
-//		switch ( order ) {
-//
-//			case 'XYZ':
-//
-//				this._y = Math.asin( clamp( m13, - 1, 1 ) );
-//
-//				if ( Math.abs( m13 ) < 0.9999999 ) {
-//
-//					this._x = Math.atan2( - m23, m33 );
-//					this._z = Math.atan2( - m12, m11 );
-//
-//				} else {
-//
-//					this._x = Math.atan2( m32, m22 );
-//					this._z = 0;
-//
-//				}
-//
-//				break;
-//
-//			case 'YXZ':
-//
-//				this._x = Math.asin( - clamp( m23, - 1, 1 ) );
-//
-//				if ( Math.abs( m23 ) < 0.9999999 ) {
-//
-//					this._y = Math.atan2( m13, m33 );
-//					this._z = Math.atan2( m21, m22 );
-//
-//				} else {
-//
-//					this._y = Math.atan2( - m31, m11 );
-//					this._z = 0;
-//
-//				}
-//
-//				break;
-//
-//			case 'ZXY':
-//
-//				this._x = Math.asin( clamp( m32, - 1, 1 ) );
-//
-//				if ( Math.abs( m32 ) < 0.9999999 ) {
-//
-//					this._y = Math.atan2( - m31, m33 );
-//					this._z = Math.atan2( - m12, m22 );
-//
-//				} else {
-//
-//					this._y = 0;
-//					this._z = Math.atan2( m21, m11 );
-//
-//				}
-//
-//				break;
-//
-//			case 'ZYX':
-//
-//				this._y = Math.asin( - clamp( m31, - 1, 1 ) );
-//
-//				if ( Math.abs( m31 ) < 0.9999999 ) {
-//
-//					this._x = Math.atan2( m32, m33 );
-//					this._z = Math.atan2( m21, m11 );
-//
-//				} else {
-//
-//					this._x = 0;
-//					this._z = Math.atan2( - m12, m22 );
-//
-//				}
-//
-//				break;
-//
-//			case 'YZX':
-//
-//				this._z = Math.asin( clamp( m21, - 1, 1 ) );
-//
-//				if ( Math.abs( m21 ) < 0.9999999 ) {
-//
-//					this._x = Math.atan2( - m23, m22 );
-//					this._y = Math.atan2( - m31, m11 );
-//
-//				} else {
-//
-//					this._x = 0;
-//					this._y = Math.atan2( m13, m33 );
-//
-//				}
-//
-//				break;
-//
-//			case 'XZY':
-//
-//				this._z = Math.asin( - clamp( m12, - 1, 1 ) );
-//
-//				if ( Math.abs( m12 ) < 0.9999999 ) {
-//
-//					this._x = Math.atan2( m32, m22 );
-//					this._y = Math.atan2( m13, m11 );
-//
-//				} else {
-//
-//					this._x = Math.atan2( - m23, m33 );
-//					this._y = 0;
-//
-//				}
-//
-//				break;
-//
-//			default:
-//
-//				console.warn( 'THREE.Euler: .setFromRotationMatrix() encountered an unknown order: ' + order );
-//
-//		}
-//
-//		this._order = order;
-//
-//		if ( update !== false ) this._onChangeCallback();
-//
-//		return this;
-//
-//	}
-//
-//	setFromQuaternion( q, order, update ) {
-//
-//		_matrix.makeRotationFromQuaternion( q );
-//
-//		return this.setFromRotationMatrix( _matrix, order, update );
-//
-//	}
-//
-//	setFromVector3( v, order ) {
-//
-//		return this.set( v.x, v.y, v.z, order || this._order );
-//
-//	}
-//
-//	reorder( newOrder ) {
-//
-//		// WARNING: this discards revolution information -bhouston
-//
-//		_quaternion.setFromEuler( this );
-//
-//		return this.setFromQuaternion( _quaternion, newOrder );
-//
-//	}
-//
-//	equals( euler ) {
-//
-//		return ( euler._x === this._x ) && ( euler._y === this._y ) && ( euler._z === this._z ) && ( euler._order === this._order );
-//	}
-//
-//	fromArray( array ) {
-//
-//		this._x = array[ 0 ];
-//		this._y = array[ 1 ];
-//		this._z = array[ 2 ];
-//		if ( array[ 3 ] !== undefined ) this._order = array[ 3 ];
-//
-//		this._onChangeCallback();
-//
-//		return this;
-//
-//	}
-//
-//	toArray( array = [], offset = 0 ) {
-//
-//		array[ offset ] = this._x;
-//		array[ offset + 1 ] = this._y;
-//		array[ offset + 2 ] = this._z;
-//		array[ offset + 3 ] = this._order;
-//
-//		return array;
-//
-//	}
-//
-//	toVector3( optionalResult ) {
+	Euler setFromRotationMatrix( Matrix4 m, String order) {
+		return setFromRotationMatrix(m, order, true);
+	}
+	
+	Euler setFromRotationMatrix( Matrix4 m, String order, boolean update ) {
+
+		// assumes the upper 3x3 of m is a pure rotation matrix (i.e, unscaled)
+
+		final double[] te = m.elements;
+		final double m11 = te[ 0 ], m12 = te[ 4 ], m13 = te[ 8 ];
+		final double m21 = te[ 1 ], m22 = te[ 5 ], m23 = te[ 9 ];
+		final double m31 = te[ 2 ], m32 = te[ 6 ], m33 = te[ 10 ];
+
+		_order = order;
+
+		switch ( order ) {
+
+			case "XYZ":
+
+				this._y = Math.asin( MathUtils.clamp( m13, - 1, 1 ) );
+
+				if ( Math.abs( m13 ) < 0.9999999 ) {
+
+					this._x = Math.atan2( - m23, m33 );
+					this._z = Math.atan2( - m12, m11 );
+
+				} else {
+
+					this._x = Math.atan2( m32, m22 );
+					this._z = 0;
+
+				}
+
+				break;
+
+			case "YXZ":
+
+				this._x = Math.asin( - MathUtils.clamp( m23, - 1, 1 ) );
+
+				if ( Math.abs( m23 ) < 0.9999999 ) {
+
+					this._y = Math.atan2( m13, m33 );
+					this._z = Math.atan2( m21, m22 );
+
+				} else {
+
+					this._y = Math.atan2( - m31, m11 );
+					this._z = 0;
+
+				}
+
+				break;
+
+			case "ZXY":
+
+				this._x = Math.asin( MathUtils.clamp( m32, - 1, 1 ) );
+
+				if ( Math.abs( m32 ) < 0.9999999 ) {
+
+					this._y = Math.atan2( - m31, m33 );
+					this._z = Math.atan2( - m12, m22 );
+
+				} else {
+
+					this._y = 0;
+					this._z = Math.atan2( m21, m11 );
+
+				}
+
+				break;
+
+			case "ZYX":
+
+				this._y = Math.asin( - MathUtils.clamp( m31, - 1, 1 ) );
+
+				if ( Math.abs( m31 ) < 0.9999999 ) {
+
+					this._x = Math.atan2( m32, m33 );
+					this._z = Math.atan2( m21, m11 );
+
+				} else {
+
+					this._x = 0;
+					this._z = Math.atan2( - m12, m22 );
+
+				}
+
+				break;
+
+			case "YZX":
+
+				this._z = Math.asin( MathUtils.clamp( m21, - 1, 1 ) );
+
+				if ( Math.abs( m21 ) < 0.9999999 ) {
+
+					this._x = Math.atan2( - m23, m22 );
+					this._y = Math.atan2( - m31, m11 );
+
+				} else {
+
+					this._x = 0;
+					this._y = Math.atan2( m13, m33 );
+
+				}
+
+				break;
+
+			case "XZY":
+
+				this._z = Math.asin( - MathUtils.clamp( m12, - 1, 1 ) );
+
+				if ( Math.abs( m12 ) < 0.9999999 ) {
+
+					this._x = Math.atan2( m32, m22 );
+					this._y = Math.atan2( m13, m11 );
+
+				} else {
+
+					this._x = Math.atan2( - m23, m33 );
+					this._y = 0;
+
+				}
+
+				break;
+
+			default:
+
+				throw new RuntimeException( "THREE.Euler: .setFromRotationMatrix() encountered an unknown order: " + order );
+
+		}
+
+		this._order = order;
+
+		if ( update ) 
+			this._onChangeCallback();
+
+		return this;
+	}
+
+	Euler setFromQuaternion(Quaternion q, String order) {
+		return setFromQuaternion(q, order, true);
+	}
+	
+	Euler setFromQuaternion( Quaternion q, String order, boolean update ) {
+
+		_matrix.makeRotationFromQuaternion( q );
+
+		return this.setFromRotationMatrix( _matrix, order, update );
+
+	}
+	
+	
+
+	Euler setFromVector3( Vector3 v, String order ) {
+
+		return this.set( v.x, v.y, v.z, order );
+
+	}
+
+	Euler reorder( String newOrder ) {
+
+		// WARNING: this discards revolution information -bhouston
+
+		_quaternion.setFromEuler( this );
+
+		return this.setFromQuaternion( _quaternion, newOrder );
+
+	}
+
+	boolean equals( Euler euler ) {
+
+		return ( euler._x == this._x ) && ( euler._y == this._y ) && ( euler._z == this._z ) && ( euler._order == this._order );
+	}
+
+	Euler fromArray( double[] array ) {
+
+		this._x = array[ 0 ];
+		this._y = array[ 1 ];
+		this._z = array[ 2 ];
+//		if ( array.length !== undefined ) 
+//			this._order = array[ 3 ];
+
+		this._onChangeCallback();
+
+		return this;
+
+	}
+	
+	double[] toArray() {
+		return toArray(new double[3], 0);
+	}
+
+	double[] toArray(double[] array) {
+		return toArray(array, 0);
+	}
+
+	double[] toArray( double[] array, int offset) {
+
+		array[ offset ] = this._x;
+		array[ offset + 1 ] = this._y;
+		array[ offset + 2 ] = this._z;
+		//array[ offset + 3 ] = this._order;
+
+		return array;
+
+	}
+
+//	Vector3 toVector3( optionalResult ) {
 //
 //		if ( optionalResult ) {
 //
@@ -321,7 +338,7 @@ public class Euler {
 //		}
 //
 //	}
-//
+
 //	_onChange( callback ) {
 //
 //		this._onChangeCallback = callback;
@@ -334,8 +351,8 @@ public class Euler {
 
 	public static final String DefaultOrder = "XYZ";
 	public static final String[] RotationOrders = new String[] { "XYZ", "YZX", "ZXY", "XZY", "YXZ", "ZYX" };
-//
-//const _matrix = /*@__PURE__*/ new Matrix4();
-//const _quaternion = /*@__PURE__*/ new Quaternion();
+
+	private static final Matrix4 _matrix = /*@__PURE__*/ new Matrix4();
+	private static final Quaternion _quaternion = /*@__PURE__*/ new Quaternion();
 
 }
