6f6b7cdc98303c21aad066f3c245ac24198f96be
Daniel P Pflager <daniel.pflager@gmail.com>
2021-01-09 11:31:39 -0800

    progress

--- src/net/three4j/math/Matrix4.java
+++ src/net/three4j/math/Matrix4.java
@@ -271,317 +271,302 @@ public class Matrix4 {
 
 	}
 
-//	lookAt( eye, target, up ) {
-//
-//		const te = this.elements;
-//
-//		_z.subVectors( eye, target );
-//
-//		if ( _z.lengthSq() == 0 ) {
-//
-//			// eye and target are in the same position
-//
-//			_z.z = 1;
-//
-//		}
-//
-//		_z.normalize();
-//		_x.crossVectors( up, _z );
-//
-//		if ( _x.lengthSq() == 0 ) {
-//
-//			// up and z are parallel
-//
-//			if ( Math.abs( up.z ) == 1 ) {
-//
-//				_z.x += 0.0001;
-//
-//			} else {
-//
-//				_z.z += 0.0001;
-//
-//			}
-//
-//			_z.normalize();
-//			_x.crossVectors( up, _z );
-//
-//		}
-//
-//		_x.normalize();
-//		_y.crossVectors( _z, _x );
-//
-//		te[ 0 ] = _x.x; te[ 4 ] = _y.x; te[ 8 ] = _z.x;
-//		te[ 1 ] = _x.y; te[ 5 ] = _y.y; te[ 9 ] = _z.y;
-//		te[ 2 ] = _x.z; te[ 6 ] = _y.z; te[ 10 ] = _z.z;
-//
-//		return this;
-//
-//	}
-//
-//	multiply( m, n ) {
-//
-//		if ( n != undefined ) {
-//
-//			console.warn( 'THREE.Matrix4: .multiply() now only accepts one argument. Use .multiplyMatrices( a, b ) instead.' );
-//			return this.multiplyMatrices( m, n );
-//
-//		}
-//
-//		return this.multiplyMatrices( this, m );
-//
-//	}
-//
-//	premultiply( m ) {
-//
-//		return this.multiplyMatrices( m, this );
-//
-//	}
-//
-//	multiplyMatrices( a, b ) {
-//
-//		const ae = a.elements;
-//		const be = b.elements;
-//		const te = this.elements;
-//
-//		const a11 = ae[ 0 ], a12 = ae[ 4 ], a13 = ae[ 8 ], a14 = ae[ 12 ];
-//		const a21 = ae[ 1 ], a22 = ae[ 5 ], a23 = ae[ 9 ], a24 = ae[ 13 ];
-//		const a31 = ae[ 2 ], a32 = ae[ 6 ], a33 = ae[ 10 ], a34 = ae[ 14 ];
-//		const a41 = ae[ 3 ], a42 = ae[ 7 ], a43 = ae[ 11 ], a44 = ae[ 15 ];
-//
-//		const b11 = be[ 0 ], b12 = be[ 4 ], b13 = be[ 8 ], b14 = be[ 12 ];
-//		const b21 = be[ 1 ], b22 = be[ 5 ], b23 = be[ 9 ], b24 = be[ 13 ];
-//		const b31 = be[ 2 ], b32 = be[ 6 ], b33 = be[ 10 ], b34 = be[ 14 ];
-//		const b41 = be[ 3 ], b42 = be[ 7 ], b43 = be[ 11 ], b44 = be[ 15 ];
-//
-//		te[ 0 ] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41;
-//		te[ 4 ] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42;
-//		te[ 8 ] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43;
-//		te[ 12 ] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44;
-//
-//		te[ 1 ] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41;
-//		te[ 5 ] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42;
-//		te[ 9 ] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43;
-//		te[ 13 ] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44;
-//
-//		te[ 2 ] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41;
-//		te[ 6 ] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42;
-//		te[ 10 ] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43;
-//		te[ 14 ] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44;
-//
-//		te[ 3 ] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41;
-//		te[ 7 ] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42;
-//		te[ 11 ] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43;
-//		te[ 15 ] = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44;
-//
-//		return this;
-//
-//	}
-//
-//	multiplyScalar( s ) {
-//
-//		const te = this.elements;
-//
-//		te[ 0 ] *= s; te[ 4 ] *= s; te[ 8 ] *= s; te[ 12 ] *= s;
-//		te[ 1 ] *= s; te[ 5 ] *= s; te[ 9 ] *= s; te[ 13 ] *= s;
-//		te[ 2 ] *= s; te[ 6 ] *= s; te[ 10 ] *= s; te[ 14 ] *= s;
-//		te[ 3 ] *= s; te[ 7 ] *= s; te[ 11 ] *= s; te[ 15 ] *= s;
-//
-//		return this;
-//
-//	}
-//
-//	determinant() {
-//
-//		const te = this.elements;
-//
-//		const n11 = te[ 0 ], n12 = te[ 4 ], n13 = te[ 8 ], n14 = te[ 12 ];
-//		const n21 = te[ 1 ], n22 = te[ 5 ], n23 = te[ 9 ], n24 = te[ 13 ];
-//		const n31 = te[ 2 ], n32 = te[ 6 ], n33 = te[ 10 ], n34 = te[ 14 ];
-//		const n41 = te[ 3 ], n42 = te[ 7 ], n43 = te[ 11 ], n44 = te[ 15 ];
-//
-//		//TODO: make this more efficient
-//		//( based on http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm )
-//
-//		return (
-//			n41 * (
-//				+ n14 * n23 * n32
-//				 - n13 * n24 * n32
-//				 - n14 * n22 * n33
-//				 + n12 * n24 * n33
-//				 + n13 * n22 * n34
-//				 - n12 * n23 * n34
-//			) +
-//			n42 * (
-//				+ n11 * n23 * n34
-//				 - n11 * n24 * n33
-//				 + n14 * n21 * n33
-//				 - n13 * n21 * n34
-//				 + n13 * n24 * n31
-//				 - n14 * n23 * n31
-//			) +
-//			n43 * (
-//				+ n11 * n24 * n32
-//				 - n11 * n22 * n34
-//				 - n14 * n21 * n32
-//				 + n12 * n21 * n34
-//				 + n14 * n22 * n31
-//				 - n12 * n24 * n31
-//			) +
-//			n44 * (
-//				- n13 * n22 * n31
-//				 - n11 * n23 * n32
-//				 + n11 * n22 * n33
-//				 + n13 * n21 * n32
-//				 - n12 * n21 * n33
-//				 + n12 * n23 * n31
-//			)
-//
-//		);
-//
-//	}
-//
-//	transpose() {
-//
-//		const te = this.elements;
-//		let tmp;
-//
-//		tmp = te[ 1 ]; te[ 1 ] = te[ 4 ]; te[ 4 ] = tmp;
-//		tmp = te[ 2 ]; te[ 2 ] = te[ 8 ]; te[ 8 ] = tmp;
-//		tmp = te[ 6 ]; te[ 6 ] = te[ 9 ]; te[ 9 ] = tmp;
-//
-//		tmp = te[ 3 ]; te[ 3 ] = te[ 12 ]; te[ 12 ] = tmp;
-//		tmp = te[ 7 ]; te[ 7 ] = te[ 13 ]; te[ 13 ] = tmp;
-//		tmp = te[ 11 ]; te[ 11 ] = te[ 14 ]; te[ 14 ] = tmp;
-//
-//		return this;
-//
-//	}
-//
-//	setPosition( x, y, z ) {
-//
-//		const te = this.elements;
-//
-//		if ( x.isVector3 ) {
-//
-//			te[ 12 ] = x.x;
-//			te[ 13 ] = x.y;
-//			te[ 14 ] = x.z;
-//
-//		} else {
-//
-//			te[ 12 ] = x;
-//			te[ 13 ] = y;
-//			te[ 14 ] = z;
-//
-//		}
-//
-//		return this;
-//
-//	}
-//
-//	invert() {
-//
-//		// based on http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm
-//		const te = this.elements,
-//
-//			n11 = te[ 0 ], n21 = te[ 1 ], n31 = te[ 2 ], n41 = te[ 3 ],
-//			n12 = te[ 4 ], n22 = te[ 5 ], n32 = te[ 6 ], n42 = te[ 7 ],
-//			n13 = te[ 8 ], n23 = te[ 9 ], n33 = te[ 10 ], n43 = te[ 11 ],
-//			n14 = te[ 12 ], n24 = te[ 13 ], n34 = te[ 14 ], n44 = te[ 15 ],
-//
-//			t11 = n23 * n34 * n42 - n24 * n33 * n42 + n24 * n32 * n43 - n22 * n34 * n43 - n23 * n32 * n44 + n22 * n33 * n44,
-//			t12 = n14 * n33 * n42 - n13 * n34 * n42 - n14 * n32 * n43 + n12 * n34 * n43 + n13 * n32 * n44 - n12 * n33 * n44,
-//			t13 = n13 * n24 * n42 - n14 * n23 * n42 + n14 * n22 * n43 - n12 * n24 * n43 - n13 * n22 * n44 + n12 * n23 * n44,
-//			t14 = n14 * n23 * n32 - n13 * n24 * n32 - n14 * n22 * n33 + n12 * n24 * n33 + n13 * n22 * n34 - n12 * n23 * n34;
-//
-//		const det = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
-//
-//		if ( det == 0 ) return this.set( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
-//
-//		const detInv = 1 / det;
-//
-//		te[ 0 ] = t11 * detInv;
-//		te[ 1 ] = ( n24 * n33 * n41 - n23 * n34 * n41 - n24 * n31 * n43 + n21 * n34 * n43 + n23 * n31 * n44 - n21 * n33 * n44 ) * detInv;
-//		te[ 2 ] = ( n22 * n34 * n41 - n24 * n32 * n41 + n24 * n31 * n42 - n21 * n34 * n42 - n22 * n31 * n44 + n21 * n32 * n44 ) * detInv;
-//		te[ 3 ] = ( n23 * n32 * n41 - n22 * n33 * n41 - n23 * n31 * n42 + n21 * n33 * n42 + n22 * n31 * n43 - n21 * n32 * n43 ) * detInv;
-//
-//		te[ 4 ] = t12 * detInv;
-//		te[ 5 ] = ( n13 * n34 * n41 - n14 * n33 * n41 + n14 * n31 * n43 - n11 * n34 * n43 - n13 * n31 * n44 + n11 * n33 * n44 ) * detInv;
-//		te[ 6 ] = ( n14 * n32 * n41 - n12 * n34 * n41 - n14 * n31 * n42 + n11 * n34 * n42 + n12 * n31 * n44 - n11 * n32 * n44 ) * detInv;
-//		te[ 7 ] = ( n12 * n33 * n41 - n13 * n32 * n41 + n13 * n31 * n42 - n11 * n33 * n42 - n12 * n31 * n43 + n11 * n32 * n43 ) * detInv;
-//
-//		te[ 8 ] = t13 * detInv;
-//		te[ 9 ] = ( n14 * n23 * n41 - n13 * n24 * n41 - n14 * n21 * n43 + n11 * n24 * n43 + n13 * n21 * n44 - n11 * n23 * n44 ) * detInv;
-//		te[ 10 ] = ( n12 * n24 * n41 - n14 * n22 * n41 + n14 * n21 * n42 - n11 * n24 * n42 - n12 * n21 * n44 + n11 * n22 * n44 ) * detInv;
-//		te[ 11 ] = ( n13 * n22 * n41 - n12 * n23 * n41 - n13 * n21 * n42 + n11 * n23 * n42 + n12 * n21 * n43 - n11 * n22 * n43 ) * detInv;
-//
-//		te[ 12 ] = t14 * detInv;
-//		te[ 13 ] = ( n13 * n24 * n31 - n14 * n23 * n31 + n14 * n21 * n33 - n11 * n24 * n33 - n13 * n21 * n34 + n11 * n23 * n34 ) * detInv;
-//		te[ 14 ] = ( n14 * n22 * n31 - n12 * n24 * n31 - n14 * n21 * n32 + n11 * n24 * n32 + n12 * n21 * n34 - n11 * n22 * n34 ) * detInv;
-//		te[ 15 ] = ( n12 * n23 * n31 - n13 * n22 * n31 + n13 * n21 * n32 - n11 * n23 * n32 - n12 * n21 * n33 + n11 * n22 * n33 ) * detInv;
-//
-//		return this;
-//
-//	}
-//
-//	scale( v ) {
-//
-//		const te = this.elements;
-//		const x = v.x, y = v.y, z = v.z;
-//
-//		te[ 0 ] *= x; te[ 4 ] *= y; te[ 8 ] *= z;
-//		te[ 1 ] *= x; te[ 5 ] *= y; te[ 9 ] *= z;
-//		te[ 2 ] *= x; te[ 6 ] *= y; te[ 10 ] *= z;
-//		te[ 3 ] *= x; te[ 7 ] *= y; te[ 11 ] *= z;
-//
-//		return this;
-//
-//	}
-//
-//	getMaxScaleOnAxis() {
-//
-//		const te = this.elements;
-//
-//		const scaleXSq = te[ 0 ] * te[ 0 ] + te[ 1 ] * te[ 1 ] + te[ 2 ] * te[ 2 ];
-//		const scaleYSq = te[ 4 ] * te[ 4 ] + te[ 5 ] * te[ 5 ] + te[ 6 ] * te[ 6 ];
-//		const scaleZSq = te[ 8 ] * te[ 8 ] + te[ 9 ] * te[ 9 ] + te[ 10 ] * te[ 10 ];
-//
-//		return Math.sqrt( Math.max( scaleXSq, scaleYSq, scaleZSq ) );
-//
-//	}
-//
-//	makeTranslation( x, y, z ) {
-//
-//		this.set(
-//
-//			1, 0, 0, x,
-//			0, 1, 0, y,
-//			0, 0, 1, z,
-//			0, 0, 0, 1
-//
-//		);
-//
-//		return this;
-//
-//	}
-//
-//	makeRotationX( theta ) {
-//
-//		const c = Math.cos( theta ), s = Math.sin( theta );
-//
-//		this.set(
-//
-//			1, 0, 0, 0,
-//			0, c, - s, 0,
-//			0, s, c, 0,
-//			0, 0, 0, 1
-//
-//		);
-//
-//		return this;
-//
-//	}
-//
+	public Matrix4 lookAt( Vector3 eye, Vector3 target, Vector3 up ) {
+
+		final double[] te = this.elements;
+
+		_z.subVectors( eye, target );
+
+		if ( _z.lengthSq() == 0 ) {
+
+			// eye and target are in the same position
+
+			_z.z = 1;
+
+		}
+
+		_z.normalize();
+		_x.crossVectors( up, _z );
+
+		if ( _x.lengthSq() == 0 ) {
+
+			// up and z are parallel
+
+			if ( Math.abs( up.z ) == 1 ) {
+
+				_z.x += 0.0001;
+
+			} else {
+
+				_z.z += 0.0001;
+
+			}
+
+			_z.normalize();
+			_x.crossVectors( up, _z );
+
+		}
+
+		_x.normalize();
+		_y.crossVectors( _z, _x );
+
+		te[ 0 ] = _x.x; te[ 4 ] = _y.x; te[ 8 ] = _z.x;
+		te[ 1 ] = _x.y; te[ 5 ] = _y.y; te[ 9 ] = _z.y;
+		te[ 2 ] = _x.z; te[ 6 ] = _y.z; te[ 10 ] = _z.z;
+
+		return this;
+
+	}
+
+	public Matrix4 multiply( Matrix4 m ) {
+
+		return this.multiplyMatrices( this, m );
+
+	}
+
+	public Matrix4 premultiply( Matrix4 m ) {
+
+		return this.multiplyMatrices( m, this );
+
+	}
+
+	public Matrix4 multiplyMatrices( Matrix4 a, Matrix4 b ) {
+
+		final double[] ae = a.elements;
+		final double[] be = b.elements;
+		final double[] te = this.elements;
+
+		final double a11 = ae[ 0 ], a12 = ae[ 4 ], a13 = ae[ 8 ], a14 = ae[ 12 ];
+		final double a21 = ae[ 1 ], a22 = ae[ 5 ], a23 = ae[ 9 ], a24 = ae[ 13 ];
+		final double a31 = ae[ 2 ], a32 = ae[ 6 ], a33 = ae[ 10 ], a34 = ae[ 14 ];
+		final double a41 = ae[ 3 ], a42 = ae[ 7 ], a43 = ae[ 11 ], a44 = ae[ 15 ];
+
+		final double b11 = be[ 0 ], b12 = be[ 4 ], b13 = be[ 8 ], b14 = be[ 12 ];
+		final double b21 = be[ 1 ], b22 = be[ 5 ], b23 = be[ 9 ], b24 = be[ 13 ];
+		final double b31 = be[ 2 ], b32 = be[ 6 ], b33 = be[ 10 ], b34 = be[ 14 ];
+		final double b41 = be[ 3 ], b42 = be[ 7 ], b43 = be[ 11 ], b44 = be[ 15 ];
+
+		te[ 0 ] = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41;
+		te[ 4 ] = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42;
+		te[ 8 ] = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43;
+		te[ 12 ] = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44;
+
+		te[ 1 ] = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41;
+		te[ 5 ] = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42;
+		te[ 9 ] = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43;
+		te[ 13 ] = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44;
+
+		te[ 2 ] = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41;
+		te[ 6 ] = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42;
+		te[ 10 ] = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43;
+		te[ 14 ] = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44;
+
+		te[ 3 ] = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41;
+		te[ 7 ] = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42;
+		te[ 11 ] = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43;
+		te[ 15 ] = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44;
+
+		return this;
+
+	}
+
+	public Matrix4 multiplyScalar( double s ) {
+
+		final double[] te = this.elements;
+
+		te[ 0 ] *= s; te[ 4 ] *= s; te[ 8 ] *= s; te[ 12 ] *= s;
+		te[ 1 ] *= s; te[ 5 ] *= s; te[ 9 ] *= s; te[ 13 ] *= s;
+		te[ 2 ] *= s; te[ 6 ] *= s; te[ 10 ] *= s; te[ 14 ] *= s;
+		te[ 3 ] *= s; te[ 7 ] *= s; te[ 11 ] *= s; te[ 15 ] *= s;
+
+		return this;
+
+	}
+
+	public double determinant() {
+
+		final double[] te = this.elements;
+
+		final double n11 = te[ 0 ], n12 = te[ 4 ], n13 = te[ 8 ], n14 = te[ 12 ];
+		final double n21 = te[ 1 ], n22 = te[ 5 ], n23 = te[ 9 ], n24 = te[ 13 ];
+		final double n31 = te[ 2 ], n32 = te[ 6 ], n33 = te[ 10 ], n34 = te[ 14 ];
+		final double n41 = te[ 3 ], n42 = te[ 7 ], n43 = te[ 11 ], n44 = te[ 15 ];
+
+		//TODO: make this more efficient
+		//( based on http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm )
+
+		return (
+			n41 * (
+				+ n14 * n23 * n32
+				 - n13 * n24 * n32
+				 - n14 * n22 * n33
+				 + n12 * n24 * n33
+				 + n13 * n22 * n34
+				 - n12 * n23 * n34
+			) +
+			n42 * (
+				+ n11 * n23 * n34
+				 - n11 * n24 * n33
+				 + n14 * n21 * n33
+				 - n13 * n21 * n34
+				 + n13 * n24 * n31
+				 - n14 * n23 * n31
+			) +
+			n43 * (
+				+ n11 * n24 * n32
+				 - n11 * n22 * n34
+				 - n14 * n21 * n32
+				 + n12 * n21 * n34
+				 + n14 * n22 * n31
+				 - n12 * n24 * n31
+			) +
+			n44 * (
+				- n13 * n22 * n31
+				 - n11 * n23 * n32
+				 + n11 * n22 * n33
+				 + n13 * n21 * n32
+				 - n12 * n21 * n33
+				 + n12 * n23 * n31
+			)
+
+		);
+
+	}
+
+	public Matrix4 transpose() {
+
+		final double[] te = this.elements;
+
+		double tmp = te[ 1 ]; te[ 1 ] = te[ 4 ]; te[ 4 ] = tmp;
+		tmp = te[ 2 ]; te[ 2 ] = te[ 8 ]; te[ 8 ] = tmp;
+		tmp = te[ 6 ]; te[ 6 ] = te[ 9 ]; te[ 9 ] = tmp;
+
+		tmp = te[ 3 ]; te[ 3 ] = te[ 12 ]; te[ 12 ] = tmp;
+		tmp = te[ 7 ]; te[ 7 ] = te[ 13 ]; te[ 13 ] = tmp;
+		tmp = te[ 11 ]; te[ 11 ] = te[ 14 ]; te[ 14 ] = tmp;
+
+		return this;
+
+	}
+
+	public Matrix4 setPosition( Vector3 v ) {
+		return setPosition(v.x, v.y, v.z);
+	}
+	
+	public Matrix4 setPosition( double x, double y, double z ) {
+
+		final double[] te = this.elements;
+
+		te[ 12 ] = x;
+		te[ 13 ] = y;
+		te[ 14 ] = z;
+
+		return this;
+
+	}
+
+	public Matrix4 invert() {
+
+		// based on http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm
+		final double[] te = this.elements;
+
+		final double n11 = te[ 0 ], n21 = te[ 1 ], n31 = te[ 2 ], n41 = te[ 3 ];
+		final double n12 = te[ 4 ], n22 = te[ 5 ], n32 = te[ 6 ], n42 = te[ 7 ];
+		final double n13 = te[ 8 ], n23 = te[ 9 ], n33 = te[ 10 ], n43 = te[ 11 ];
+		final double n14 = te[ 12 ], n24 = te[ 13 ], n34 = te[ 14 ], n44 = te[ 15 ];
+
+		final double t11 = n23 * n34 * n42 - n24 * n33 * n42 + n24 * n32 * n43 - n22 * n34 * n43 - n23 * n32 * n44 + n22 * n33 * n44;
+		final double t12 = n14 * n33 * n42 - n13 * n34 * n42 - n14 * n32 * n43 + n12 * n34 * n43 + n13 * n32 * n44 - n12 * n33 * n44;
+		final double t13 = n13 * n24 * n42 - n14 * n23 * n42 + n14 * n22 * n43 - n12 * n24 * n43 - n13 * n22 * n44 + n12 * n23 * n44;
+		final double t14 = n14 * n23 * n32 - n13 * n24 * n32 - n14 * n22 * n33 + n12 * n24 * n33 + n13 * n22 * n34 - n12 * n23 * n34;
+
+		final double  det = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
+
+		if ( det == 0 ) return this.set( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
+
+		final double  detInv = 1 / det;
+
+		te[ 0 ] = t11 * detInv;
+		te[ 1 ] = ( n24 * n33 * n41 - n23 * n34 * n41 - n24 * n31 * n43 + n21 * n34 * n43 + n23 * n31 * n44 - n21 * n33 * n44 ) * detInv;
+		te[ 2 ] = ( n22 * n34 * n41 - n24 * n32 * n41 + n24 * n31 * n42 - n21 * n34 * n42 - n22 * n31 * n44 + n21 * n32 * n44 ) * detInv;
+		te[ 3 ] = ( n23 * n32 * n41 - n22 * n33 * n41 - n23 * n31 * n42 + n21 * n33 * n42 + n22 * n31 * n43 - n21 * n32 * n43 ) * detInv;
+
+		te[ 4 ] = t12 * detInv;
+		te[ 5 ] = ( n13 * n34 * n41 - n14 * n33 * n41 + n14 * n31 * n43 - n11 * n34 * n43 - n13 * n31 * n44 + n11 * n33 * n44 ) * detInv;
+		te[ 6 ] = ( n14 * n32 * n41 - n12 * n34 * n41 - n14 * n31 * n42 + n11 * n34 * n42 + n12 * n31 * n44 - n11 * n32 * n44 ) * detInv;
+		te[ 7 ] = ( n12 * n33 * n41 - n13 * n32 * n41 + n13 * n31 * n42 - n11 * n33 * n42 - n12 * n31 * n43 + n11 * n32 * n43 ) * detInv;
+
+		te[ 8 ] = t13 * detInv;
+		te[ 9 ] = ( n14 * n23 * n41 - n13 * n24 * n41 - n14 * n21 * n43 + n11 * n24 * n43 + n13 * n21 * n44 - n11 * n23 * n44 ) * detInv;
+		te[ 10 ] = ( n12 * n24 * n41 - n14 * n22 * n41 + n14 * n21 * n42 - n11 * n24 * n42 - n12 * n21 * n44 + n11 * n22 * n44 ) * detInv;
+		te[ 11 ] = ( n13 * n22 * n41 - n12 * n23 * n41 - n13 * n21 * n42 + n11 * n23 * n42 + n12 * n21 * n43 - n11 * n22 * n43 ) * detInv;
+
+		te[ 12 ] = t14 * detInv;
+		te[ 13 ] = ( n13 * n24 * n31 - n14 * n23 * n31 + n14 * n21 * n33 - n11 * n24 * n33 - n13 * n21 * n34 + n11 * n23 * n34 ) * detInv;
+		te[ 14 ] = ( n14 * n22 * n31 - n12 * n24 * n31 - n14 * n21 * n32 + n11 * n24 * n32 + n12 * n21 * n34 - n11 * n22 * n34 ) * detInv;
+		te[ 15 ] = ( n12 * n23 * n31 - n13 * n22 * n31 + n13 * n21 * n32 - n11 * n23 * n32 - n12 * n21 * n33 + n11 * n22 * n33 ) * detInv;
+
+		return this;
+
+	}
+
+	public Matrix4 scale( Vector3 v ) {
+
+		final double[] te = this.elements;
+		final double  x = v.x, y = v.y, z = v.z;
+
+		te[ 0 ] *= x; te[ 4 ] *= y; te[ 8 ] *= z;
+		te[ 1 ] *= x; te[ 5 ] *= y; te[ 9 ] *= z;
+		te[ 2 ] *= x; te[ 6 ] *= y; te[ 10 ] *= z;
+		te[ 3 ] *= x; te[ 7 ] *= y; te[ 11 ] *= z;
+
+		return this;
+
+	}
+
+	public double getMaxScaleOnAxis() {
+
+		final double[] te = this.elements;
+
+		final double scaleXSq = te[ 0 ] * te[ 0 ] + te[ 1 ] * te[ 1 ] + te[ 2 ] * te[ 2 ];
+		final double scaleYSq = te[ 4 ] * te[ 4 ] + te[ 5 ] * te[ 5 ] + te[ 6 ] * te[ 6 ];
+		final double scaleZSq = te[ 8 ] * te[ 8 ] + te[ 9 ] * te[ 9 ] + te[ 10 ] * te[ 10 ];
+
+		return Math.sqrt( Math.max( Math.max( scaleXSq, scaleYSq), scaleZSq ) );
+	}
+
+	public Matrix4 makeTranslation( double x, double y, double z ) {
+
+		this.set(
+
+			1, 0, 0, x,
+			0, 1, 0, y,
+			0, 0, 1, z,
+			0, 0, 0, 1
+
+		);
+
+		return this;
+
+	}
+
+	public Matrix4 makeRotationX( double theta ) {
+
+		final double c = Math.cos( theta ), s = Math.sin( theta );
+
+		this.set(
+
+			1, 0, 0, 0,
+			0, c, - s, 0,
+			0, s, c, 0,
+			0, 0, 0, 1
+
+		);
+
+		return this;
+
+	}
+
 //	makeRotationY( theta ) {
 //
 //		const c = Math.cos( theta ), s = Math.sin( theta );
