23db8b2d5afc9ff9a63a2ca3ec759b9b6092ee8d
Daniel P Pflager <daniel.pflager@gmail.com>
2021-01-09 10:53:07 -0800

    progress

--- src/net/three4j/math/Matrix4.java
+++ src/net/three4j/math/Matrix4.java
@@ -1,7 +1,8 @@
 package net.three4j.math;
 
 //import { Vector3 } from './Vector3.js';
-//
+import static net.three4j.THREE.console;
+
 public class Matrix4 {
 	
 	public double[] elements;
@@ -17,252 +18,253 @@ public class Matrix4 {
 		};
 	}
 
-//	set( n11, n12, n13, n14, n21, n22, n23, n24, n31, n32, n33, n34, n41, n42, n43, n44 ) {
-//
-//		const te = this.elements;
-//
-//		te[ 0 ] = n11; te[ 4 ] = n12; te[ 8 ] = n13; te[ 12 ] = n14;
-//		te[ 1 ] = n21; te[ 5 ] = n22; te[ 9 ] = n23; te[ 13 ] = n24;
-//		te[ 2 ] = n31; te[ 6 ] = n32; te[ 10 ] = n33; te[ 14 ] = n34;
-//		te[ 3 ] = n41; te[ 7 ] = n42; te[ 11 ] = n43; te[ 15 ] = n44;
-//
-//		return this;
-//
-//	}
-//
-//	identity() {
-//
-//		this.set(
-//
-//			1, 0, 0, 0,
-//			0, 1, 0, 0,
-//			0, 0, 1, 0,
-//			0, 0, 0, 1
-//
-//		);
-//
-//		return this;
-//
-//	}
-//
-//	clone() {
-//
-//		return new Matrix4().fromArray( this.elements );
-//
-//	}
-//
-//	copy( m ) {
-//
-//		const te = this.elements;
-//		const me = m.elements;
-//
-//		te[ 0 ] = me[ 0 ]; te[ 1 ] = me[ 1 ]; te[ 2 ] = me[ 2 ]; te[ 3 ] = me[ 3 ];
-//		te[ 4 ] = me[ 4 ]; te[ 5 ] = me[ 5 ]; te[ 6 ] = me[ 6 ]; te[ 7 ] = me[ 7 ];
-//		te[ 8 ] = me[ 8 ]; te[ 9 ] = me[ 9 ]; te[ 10 ] = me[ 10 ]; te[ 11 ] = me[ 11 ];
-//		te[ 12 ] = me[ 12 ]; te[ 13 ] = me[ 13 ]; te[ 14 ] = me[ 14 ]; te[ 15 ] = me[ 15 ];
-//
-//		return this;
-//
-//	}
-//
-//	copyPosition( m ) {
-//
-//		const te = this.elements, me = m.elements;
-//
-//		te[ 12 ] = me[ 12 ];
-//		te[ 13 ] = me[ 13 ];
-//		te[ 14 ] = me[ 14 ];
-//
-//		return this;
-//
-//	}
-//
-//	extractBasis( xAxis, yAxis, zAxis ) {
-//
-//		xAxis.setFromMatrixColumn( this, 0 );
-//		yAxis.setFromMatrixColumn( this, 1 );
-//		zAxis.setFromMatrixColumn( this, 2 );
-//
-//		return this;
-//
-//	}
-//
-//	makeBasis( xAxis, yAxis, zAxis ) {
-//
-//		this.set(
-//			xAxis.x, yAxis.x, zAxis.x, 0,
-//			xAxis.y, yAxis.y, zAxis.y, 0,
-//			xAxis.z, yAxis.z, zAxis.z, 0,
-//			0, 0, 0, 1
-//		);
-//
-//		return this;
-//
-//	}
-//
-//	extractRotation( m ) {
-//
-//		// this method does not support reflection matrices
-//
-//		const te = this.elements;
-//		const me = m.elements;
-//
-//		const scaleX = 1 / _v1.setFromMatrixColumn( m, 0 ).length();
-//		const scaleY = 1 / _v1.setFromMatrixColumn( m, 1 ).length();
-//		const scaleZ = 1 / _v1.setFromMatrixColumn( m, 2 ).length();
-//
-//		te[ 0 ] = me[ 0 ] * scaleX;
-//		te[ 1 ] = me[ 1 ] * scaleX;
-//		te[ 2 ] = me[ 2 ] * scaleX;
-//		te[ 3 ] = 0;
-//
-//		te[ 4 ] = me[ 4 ] * scaleY;
-//		te[ 5 ] = me[ 5 ] * scaleY;
-//		te[ 6 ] = me[ 6 ] * scaleY;
-//		te[ 7 ] = 0;
-//
-//		te[ 8 ] = me[ 8 ] * scaleZ;
-//		te[ 9 ] = me[ 9 ] * scaleZ;
-//		te[ 10 ] = me[ 10 ] * scaleZ;
-//		te[ 11 ] = 0;
-//
-//		te[ 12 ] = 0;
-//		te[ 13 ] = 0;
-//		te[ 14 ] = 0;
-//		te[ 15 ] = 1;
-//
-//		return this;
-//
-//	}
-//
-//	makeRotationFromEuler( euler ) {
-//
-//		if ( ! ( euler && euler.isEuler ) ) {
-//
-//			console.error( 'THREE.Matrix4: .makeRotationFromEuler() now expects a Euler rotation rather than a Vector3 and order.' );
-//
-//		}
-//
-//		const te = this.elements;
-//
-//		const x = euler.x, y = euler.y, z = euler.z;
-//		const a = Math.cos( x ), b = Math.sin( x );
-//		const c = Math.cos( y ), d = Math.sin( y );
-//		const e = Math.cos( z ), f = Math.sin( z );
-//
-//		if ( euler.order === 'XYZ' ) {
-//
-//			const ae = a * e, af = a * f, be = b * e, bf = b * f;
-//
-//			te[ 0 ] = c * e;
-//			te[ 4 ] = - c * f;
-//			te[ 8 ] = d;
-//
-//			te[ 1 ] = af + be * d;
-//			te[ 5 ] = ae - bf * d;
-//			te[ 9 ] = - b * c;
-//
-//			te[ 2 ] = bf - ae * d;
-//			te[ 6 ] = be + af * d;
-//			te[ 10 ] = a * c;
-//
-//		} else if ( euler.order === 'YXZ' ) {
-//
-//			const ce = c * e, cf = c * f, de = d * e, df = d * f;
-//
-//			te[ 0 ] = ce + df * b;
-//			te[ 4 ] = de * b - cf;
-//			te[ 8 ] = a * d;
-//
-//			te[ 1 ] = a * f;
-//			te[ 5 ] = a * e;
-//			te[ 9 ] = - b;
-//
-//			te[ 2 ] = cf * b - de;
-//			te[ 6 ] = df + ce * b;
-//			te[ 10 ] = a * c;
-//
-//		} else if ( euler.order === 'ZXY' ) {
-//
-//			const ce = c * e, cf = c * f, de = d * e, df = d * f;
-//
-//			te[ 0 ] = ce - df * b;
-//			te[ 4 ] = - a * f;
-//			te[ 8 ] = de + cf * b;
-//
-//			te[ 1 ] = cf + de * b;
-//			te[ 5 ] = a * e;
-//			te[ 9 ] = df - ce * b;
-//
-//			te[ 2 ] = - a * d;
-//			te[ 6 ] = b;
-//			te[ 10 ] = a * c;
-//
-//		} else if ( euler.order === 'ZYX' ) {
-//
-//			const ae = a * e, af = a * f, be = b * e, bf = b * f;
-//
-//			te[ 0 ] = c * e;
-//			te[ 4 ] = be * d - af;
-//			te[ 8 ] = ae * d + bf;
-//
-//			te[ 1 ] = c * f;
-//			te[ 5 ] = bf * d + ae;
-//			te[ 9 ] = af * d - be;
-//
-//			te[ 2 ] = - d;
-//			te[ 6 ] = b * c;
-//			te[ 10 ] = a * c;
-//
-//		} else if ( euler.order === 'YZX' ) {
-//
-//			const ac = a * c, ad = a * d, bc = b * c, bd = b * d;
-//
-//			te[ 0 ] = c * e;
-//			te[ 4 ] = bd - ac * f;
-//			te[ 8 ] = bc * f + ad;
-//
-//			te[ 1 ] = f;
-//			te[ 5 ] = a * e;
-//			te[ 9 ] = - b * e;
-//
-//			te[ 2 ] = - d * e;
-//			te[ 6 ] = ad * f + bc;
-//			te[ 10 ] = ac - bd * f;
-//
-//		} else if ( euler.order === 'XZY' ) {
-//
-//			const ac = a * c, ad = a * d, bc = b * c, bd = b * d;
-//
-//			te[ 0 ] = c * e;
-//			te[ 4 ] = - f;
-//			te[ 8 ] = d * e;
-//
-//			te[ 1 ] = ac * f + bd;
-//			te[ 5 ] = a * e;
-//			te[ 9 ] = ad * f - bc;
-//
-//			te[ 2 ] = bc * f - ad;
-//			te[ 6 ] = b * e;
-//			te[ 10 ] = bd * f + ac;
-//
-//		}
-//
-//		// bottom row
-//		te[ 3 ] = 0;
-//		te[ 7 ] = 0;
-//		te[ 11 ] = 0;
-//
-//		// last column
-//		te[ 12 ] = 0;
-//		te[ 13 ] = 0;
-//		te[ 14 ] = 0;
-//		te[ 15 ] = 1;
-//
-//		return this;
-//
-//	}
-//
+	public Matrix4 set( double n11, double n12, double n13, double n14, double n21, double n22, double n23, double n24, double n31, double n32, double n33, double n34, double n41, double n42, double n43, double n44 ) {
+
+		final double[] te = this.elements;
+
+		// DPP: Column Major
+		te[ 0 ] = n11; te[ 4 ] = n12; te[ 8 ] = n13; te[ 12 ] = n14;
+		te[ 1 ] = n21; te[ 5 ] = n22; te[ 9 ] = n23; te[ 13 ] = n24;
+		te[ 2 ] = n31; te[ 6 ] = n32; te[ 10 ] = n33; te[ 14 ] = n34;
+		te[ 3 ] = n41; te[ 7 ] = n42; te[ 11 ] = n43; te[ 15 ] = n44;
+
+		return this;
+
+	}
+
+	public Matrix4 identity() {
+
+		this.set(
+
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+
+		);
+
+		return this;
+
+	}
+
+	public Matrix4 clone() {
+
+		return new Matrix4().fromArray( this.elements );
+
+	}
+
+	Matrix4 copy( Matrix4 m ) {
+
+		final double[] te = this.elements;
+		final double[] me = m.elements;
+
+		te[ 0 ] = me[ 0 ]; te[ 1 ] = me[ 1 ]; te[ 2 ] = me[ 2 ]; te[ 3 ] = me[ 3 ];
+		te[ 4 ] = me[ 4 ]; te[ 5 ] = me[ 5 ]; te[ 6 ] = me[ 6 ]; te[ 7 ] = me[ 7 ];
+		te[ 8 ] = me[ 8 ]; te[ 9 ] = me[ 9 ]; te[ 10 ] = me[ 10 ]; te[ 11 ] = me[ 11 ];
+		te[ 12 ] = me[ 12 ]; te[ 13 ] = me[ 13 ]; te[ 14 ] = me[ 14 ]; te[ 15 ] = me[ 15 ];
+
+		return this;
+
+	}
+
+	public Matrix4 copyPosition( Matrix4 m ) {
+
+		final double[] te = this.elements, me = m.elements;
+
+		te[ 12 ] = me[ 12 ];
+		te[ 13 ] = me[ 13 ];
+		te[ 14 ] = me[ 14 ];
+
+		return this;
+
+	}
+
+	public Matrix4 extractBasis( Vector3 xAxis, Vector3 yAxis, Vector3 zAxis ) {
+
+		xAxis.setFromMatrixColumn( this, 0 );
+		yAxis.setFromMatrixColumn( this, 1 );
+		zAxis.setFromMatrixColumn( this, 2 );
+
+		return this;
+
+	}
+
+	public Matrix4 makeBasis( Vector3 xAxis, Vector3 yAxis, Vector3 zAxis ) {
+
+		this.set(
+			xAxis.x, yAxis.x, zAxis.x, 0,
+			xAxis.y, yAxis.y, zAxis.y, 0,
+			xAxis.z, yAxis.z, zAxis.z, 0,
+			0, 0, 0, 1
+		);
+
+		return this;
+
+	}
+
+	public Matrix4 extractRotation( Matrix4 m ) {
+
+		// this method does not support reflection matrices
+
+		final double[] te = this.elements;
+		final double[] me = m.elements;
+
+		final double scaleX = 1 / _v1.setFromMatrixColumn( m, 0 ).length();
+		final double scaleY = 1 / _v1.setFromMatrixColumn( m, 1 ).length();
+		final double scaleZ = 1 / _v1.setFromMatrixColumn( m, 2 ).length();
+
+		te[ 0 ] = me[ 0 ] * scaleX;
+		te[ 1 ] = me[ 1 ] * scaleX;
+		te[ 2 ] = me[ 2 ] * scaleX;
+		te[ 3 ] = 0;
+
+		te[ 4 ] = me[ 4 ] * scaleY;
+		te[ 5 ] = me[ 5 ] * scaleY;
+		te[ 6 ] = me[ 6 ] * scaleY;
+		te[ 7 ] = 0;
+
+		te[ 8 ] = me[ 8 ] * scaleZ;
+		te[ 9 ] = me[ 9 ] * scaleZ;
+		te[ 10 ] = me[ 10 ] * scaleZ;
+		te[ 11 ] = 0;
+
+		te[ 12 ] = 0;
+		te[ 13 ] = 0;
+		te[ 14 ] = 0;
+		te[ 15 ] = 1;
+
+		return this;
+
+	}
+
+	public Matrix4 makeRotationFromEuler( Euler euler ) {
+
+		if ( ! ( euler != null && euler.isEuler ) ) {
+
+			console.error( "THREE.Matrix4: .makeRotationFromEuler() now expects a Euler rotation rather than a Vector3 and order." );
+
+		}
+
+		final double[] te = this.elements;
+
+		final double x = euler.x(), y = euler.y(), z = euler.z();
+		final double a = Math.cos( x ), b = Math.sin( x );
+		final double c = Math.cos( y ), d = Math.sin( y );
+		final double e = Math.cos( z ), f = Math.sin( z );
+
+		if ( euler.order() == "XYZ" ) {
+
+			final double ae = a * e, af = a * f, be = b * e, bf = b * f;
+
+			te[ 0 ] = c * e;
+			te[ 4 ] = - c * f;
+			te[ 8 ] = d;
+
+			te[ 1 ] = af + be * d;
+			te[ 5 ] = ae - bf * d;
+			te[ 9 ] = - b * c;
+
+			te[ 2 ] = bf - ae * d;
+			te[ 6 ] = be + af * d;
+			te[ 10 ] = a * c;
+
+		} else if ( euler.order() == "YXZ" ) {
+
+			final double ce = c * e, cf = c * f, de = d * e, df = d * f;
+
+			te[ 0 ] = ce + df * b;
+			te[ 4 ] = de * b - cf;
+			te[ 8 ] = a * d;
+
+			te[ 1 ] = a * f;
+			te[ 5 ] = a * e;
+			te[ 9 ] = - b;
+
+			te[ 2 ] = cf * b - de;
+			te[ 6 ] = df + ce * b;
+			te[ 10 ] = a * c;
+
+		} else if ( euler.order() == "ZXY" ) {
+
+			final double ce = c * e, cf = c * f, de = d * e, df = d * f;
+
+			te[ 0 ] = ce - df * b;
+			te[ 4 ] = - a * f;
+			te[ 8 ] = de + cf * b;
+
+			te[ 1 ] = cf + de * b;
+			te[ 5 ] = a * e;
+			te[ 9 ] = df - ce * b;
+
+			te[ 2 ] = - a * d;
+			te[ 6 ] = b;
+			te[ 10 ] = a * c;
+
+		} else if ( euler.order() == "ZYX" ) {
+
+			final double ae = a * e, af = a * f, be = b * e, bf = b * f;
+
+			te[ 0 ] = c * e;
+			te[ 4 ] = be * d - af;
+			te[ 8 ] = ae * d + bf;
+
+			te[ 1 ] = c * f;
+			te[ 5 ] = bf * d + ae;
+			te[ 9 ] = af * d - be;
+
+			te[ 2 ] = - d;
+			te[ 6 ] = b * c;
+			te[ 10 ] = a * c;
+
+		} else if ( euler.order() == "YZX" ) {
+
+			final double ac = a * c, ad = a * d, bc = b * c, bd = b * d;
+
+			te[ 0 ] = c * e;
+			te[ 4 ] = bd - ac * f;
+			te[ 8 ] = bc * f + ad;
+
+			te[ 1 ] = f;
+			te[ 5 ] = a * e;
+			te[ 9 ] = - b * e;
+
+			te[ 2 ] = - d * e;
+			te[ 6 ] = ad * f + bc;
+			te[ 10 ] = ac - bd * f;
+
+		} else if ( euler.order() == "XZY" ) {
+
+			final double ac = a * c, ad = a * d, bc = b * c, bd = b * d;
+
+			te[ 0 ] = c * e;
+			te[ 4 ] = - f;
+			te[ 8 ] = d * e;
+
+			te[ 1 ] = ac * f + bd;
+			te[ 5 ] = a * e;
+			te[ 9 ] = ad * f - bc;
+
+			te[ 2 ] = bc * f - ad;
+			te[ 6 ] = b * e;
+			te[ 10 ] = bd * f + ac;
+
+		}
+
+		// bottom row
+		te[ 3 ] = 0;
+		te[ 7 ] = 0;
+		te[ 11 ] = 0;
+
+		// last column
+		te[ 12 ] = 0;
+		te[ 13 ] = 0;
+		te[ 14 ] = 0;
+		te[ 15 ] = 1;
+
+		return this;
+
+	}
+
 	Matrix4 makeRotationFromQuaternion( Quaternion q ) {
 
 		return this.compose( _zero, q, _one );
@@ -275,7 +277,7 @@ public class Matrix4 {
 //
 //		_z.subVectors( eye, target );
 //
-//		if ( _z.lengthSq() === 0 ) {
+//		if ( _z.lengthSq() == 0 ) {
 //
 //			// eye and target are in the same position
 //
@@ -286,11 +288,11 @@ public class Matrix4 {
 //		_z.normalize();
 //		_x.crossVectors( up, _z );
 //
-//		if ( _x.lengthSq() === 0 ) {
+//		if ( _x.lengthSq() == 0 ) {
 //
 //			// up and z are parallel
 //
-//			if ( Math.abs( up.z ) === 1 ) {
+//			if ( Math.abs( up.z ) == 1 ) {
 //
 //				_z.x += 0.0001;
 //
@@ -318,7 +320,7 @@ public class Matrix4 {
 //
 //	multiply( m, n ) {
 //
-//		if ( n !== undefined ) {
+//		if ( n != undefined ) {
 //
 //			console.warn( 'THREE.Matrix4: .multiply() now only accepts one argument. Use .multiplyMatrices( a, b ) instead.' );
 //			return this.multiplyMatrices( m, n );
@@ -494,7 +496,7 @@ public class Matrix4 {
 //
 //		const det = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
 //
-//		if ( det === 0 ) return this.set( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
+//		if ( det == 0 ) return this.set( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
 //
 //		const detInv = 1 / det;
 //
@@ -750,7 +752,7 @@ public class Matrix4 {
 //
 //	makePerspective( left, right, top, bottom, near, far ) {
 //
-//		if ( far === undefined ) {
+//		if ( far == undefined ) {
 //
 //			console.warn( 'THREE.Matrix4: .makePerspective() has been redefined and has a new signature. Please check the docs.' );
 //
@@ -783,7 +785,7 @@ public class Matrix4 {
 //
 //		const x = ( right + left ) * w;
 //		const y = ( top + bottom ) * h;
-//		const z = ( far + near ) * p;
+//		const z = ( far + near ) * p;final double
 //
 //		te[ 0 ] = 2 * w;	te[ 4 ] = 0;	te[ 8 ] = 0;	te[ 12 ] = - x;
 //		te[ 1 ] = 0;	te[ 5 ] = 2 * h;	te[ 9 ] = 0;	te[ 13 ] = - y;
@@ -801,7 +803,7 @@ public class Matrix4 {
 //
 //		for ( let i = 0; i < 16; i ++ ) {
 //
-//			if ( te[ i ] !== me[ i ] ) return false;
+//			if ( te[ i ] != me[ i ] ) return false;
 //
 //		}
 //
