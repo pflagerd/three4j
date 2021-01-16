cab492fd556c1ed5eb28d489049066e044c09d73
Daniel P Pflager <daniel.pflager@gmail.com>
2021-01-15 23:26:47 -0800

    Add an overload for cases I'm seeing.

--- src/net/three4j/Console.java
+++ src/net/three4j/Console.java
@@ -4,9 +4,18 @@ public class Console {
 	public void warn(String s) {		
 	}
 	
+	public void warn(String s, int i) {
+		warn(s + i);
+	}
+
+	
 	public void error(String s) {
 		
 	}
-	
+
+	public void error(String s, int i) {
+		error(s + i);
+	}
+
 	
 }
