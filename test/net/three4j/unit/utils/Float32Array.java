191a4c65c504513d483edfe77036702019429e27
Daniel P Pflager <daniel.pflager@gmail.com>
2021-01-15 23:28:17 -0800

    progress

--- test/net/three4j/unit/utils/Float32Array.java
+++ test/net/three4j/unit/utils/Float32Array.java
@@ -5,17 +5,38 @@ import java.util.Arrays;
 public class Float32Array extends TypedArray {
 	private double[] _array;
 	
+	public Float32Array(int size) {
+		super(size);
+		_array = new double[size];
+	}
+	
 	public Float32Array(double[] array) {
 		super(array.length);
 		_array = Arrays.copyOf(array, array.length);
 	}
 	
 	public double[] array() {
-		return Arrays.copyOf(_array, _array.length);
+		return _array;
+	}
+	
+	public void set(double[] array) {
+		set(array, 0);
+	}
+
+	public void set(double[] array, int offset) {
+		for (int i = 0; i < Math.min(_array.length, array.length); i++) {
+			_array[i + offset] = array[i];
+		}
 	}
 
 	@Override
 	public Float32Array clone() {
 		return new Float32Array(_array);
 	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " { _array : " + Arrays.toString(_array) + " }";
+	}
+
 }
