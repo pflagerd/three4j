9cfd693857aa2f2e5df6476938c508fbe1dcaff2
Daniel P Pflager <daniel.pflager@gmail.com>
2021-01-17 15:41:07 -0800

    hardly anything

---
+++ test/net/three4j/unit/src/lights/LightTests.java
@@ -0,0 +1,72 @@
+package net.three4j.unit.src.lights;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+//import { runStdLightTests } from '../../utils/qunit-utils';
+
+public class LightTests {
+	
+//	QUnit.module( 'Light', ( hooks ) => {
+//
+//		var lights = undefined;
+//		hooks.beforeEach( function () {
+//
+//			const parameters = {
+//				color: 0xaaaaaa,
+//				intensity: 0.5
+//			};
+//
+//			lights = [
+//				new Light(),
+//				new Light( parameters.color ),
+//				new Light( parameters.color, parameters.intensity )
+//			];
+//
+//		} );
+//
+//		// INHERITANCE
+//		QUnit.todo( "Extending", ( assert ) => {
+//
+//			assert.ok( false, "everything's gonna be alright" );
+//
+//		} );
+//
+//		// INSTANCING
+//		QUnit.todo( "Instancing", ( assert ) => {
+//
+//			assert.ok( false, "everything's gonna be alright" );
+//
+//		} );
+//
+//		// PUBLIC STUFF
+//		QUnit.todo( "isLight", ( assert ) => {
+//
+//			assert.ok( false, "everything's gonna be alright" );
+//
+//		} );
+//
+//		QUnit.todo( "copy", ( assert ) => {
+//
+//			assert.ok( false, "everything's gonna be alright" );
+//
+//		} );
+//
+//		QUnit.todo( "toJSON", ( assert ) => {
+//
+//			assert.ok( false, "everything's gonna be alright" );
+//
+//		} );
+
+		// OTHERS
+		@Test
+		public void StandardLightTests() {
+
+			//runStdLightTests( assert, lights );
+
+		}
+
+}
